25b24a6f6decaf7ab2dbbaaa4505948ec98350fb
thenurinethangi <thenurinathangi@gmail.com>
2025-06-10 20:00:40 +0530

    EmployeeServlet new id generate function implement

--- EMS/Employee-Management-System-Backend/src/main/java/org/example/EmployeeServlet.java
+++ EMS/Employee-Management-System-Backend/src/main/java/org/example/EmployeeServlet.java
@@ -24,40 +24,81 @@ public class EmployeeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         resp.setContentType("application/json");
-
+        Connection connection = null;
         try {
             BasicDataSource basicDataSource = (BasicDataSource) req.getServletContext().getAttribute("dataSource");
-            Connection connection = basicDataSource.getConnection();
-            PreparedStatement pst = connection.prepareStatement("select * from employee");
-            ResultSet result = pst.executeQuery();
+            connection = basicDataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
-            List<Map<String,String>> employees = new ArrayList<>();
-            while(result.next()){
-               Map<String,String> em = Map.of(
-                       "id",result.getString(1),
-                       "name",result.getString(2),
-                       "address",result.getString(3),
-                       "phoneNo",result.getString(4),
-                       "salary",String.valueOf(result.getDouble(5))
-               );
-               employees.add(em);
+        if(req.getParameter("newid")!=null){
+            try {
+                PreparedStatement pst = connection.prepareStatement("select * from employee order by id desc limit 1");
+                ResultSet result = pst.executeQuery();
+                String newId;
+                if(result.next()){
+                  String lastId = result.getString(1);
+                  String noPart = lastId.substring(2);
+                  int no = Integer.parseInt(noPart);
+                  no++;
+                  newId = String.format("E-%04d",no);
+
+                }
+                else{
+                    newId = "E-0001";
+                }
+                resp.setStatus(HttpServletResponse.SC_OK);
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.writeValue(resp.getWriter(), Map.of("code", "200",
+                        "status", "ok",
+                        "message", "success!",
+                        "data", newId)
+                );
+
+            } catch (SQLException e) {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.writeValue(resp.getWriter(), Map.of("code", "500",
+                                                                "status", "error",
+                                                                "message", "internal server error!"));
+                throw new RuntimeException(e);
             }
 
-            resp.setStatus(HttpServletResponse.SC_OK);
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.writeValue(resp.getWriter(),Map.of("code", "200",
-                                                            "status", "ok",
-                                                            "message", "success!",
-                                                                "data",employees)
-            );
+        }
+        else {
+            try {
+                PreparedStatement pst = connection.prepareStatement("select * from employee");
+                ResultSet result = pst.executeQuery();
 
-        } catch (SQLException e) {
-            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.writeValue(resp.getWriter(),Map.of("code", "500",
-                                                            "status", "error",
-                                                            "message", "internal server error!"));
-            throw new RuntimeException(e);
+                List<Map<String, String>> employees = new ArrayList<>();
+                while (result.next()) {
+                    Map<String, String> em = Map.of(
+                            "id", result.getString(1),
+                            "name", result.getString(2),
+                            "address", result.getString(3),
+                            "phoneNo", result.getString(4),
+                            "salary", String.valueOf(result.getDouble(5))
+                    );
+                    employees.add(em);
+                }
+
+                resp.setStatus(HttpServletResponse.SC_OK);
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.writeValue(resp.getWriter(), Map.of("code", "200",
+                        "status", "ok",
+                        "message", "success!",
+                        "data", employees)
+                );
+
+            } catch (SQLException e) {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.writeValue(resp.getWriter(), Map.of("code", "500",
+                        "status", "error",
+                        "message", "internal server error!"));
+                throw new RuntimeException(e);
+            }
         }
     }
 
